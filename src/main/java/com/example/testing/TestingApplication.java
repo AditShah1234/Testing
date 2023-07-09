b851d2eb26388a63b3fa5a87fbec30fe7a9d0b0d
AditShah1234 <48185533+AditShah1234@users.noreply.github.com>
2023-07-09 18:25:59 -0400

    Inputs

--- src/main/java/com/example/testing/TestingApplication.java
+++ src/main/java/com/example/testing/TestingApplication.java
@@ -3,11 +3,26 @@ package com.example.testing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+import com.example.testing.fun.Control;
 @SpringBootApplication
 public class TestingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestingApplication.class, args);
-	}
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the size of the floor: ");
+		int size = scanner.nextInt();
+		Control robo = new Control();
+		robo.setBoard(size);
 
+
+		while (true) {
+			System.out.print("Enter command: ");
+			String command = scanner.nextLine();
+
+			robo.commandCenter(command);
+
+	}
+	}
 }
